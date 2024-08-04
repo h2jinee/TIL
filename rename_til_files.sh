5086a0c02fd1d2171c95d5d39c0a630bb17f0e95
h2jinee <wjsgmlwls97@gmail.com>
2024-08-04 16:08:14 +0900

    Rename TIL files to maintain proper sorting order

---
+++ rename_til_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 파일 확장자 및 접미사 정의
+suffix="TIL.md"
+
+for i in {1..14}
+do
+  old_name="${i}일차 ${suffix}"
+  new_name=$(printf "%02d일차 %s" $i "${suffix}")
+  if [ -e "$old_name" ]; then
+    git mv "$old_name" "$new_name"
+  fi
+done
